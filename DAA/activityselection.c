acbe3ebcbead8dc007a02b8da15b6a9fc2a321b6
Rsan25 <roshan25122005@gmail.com>
2025-10-28 23:51:52 +0530

    Added new files

--- DAA/activityselection.c
+++ DAA/activityselection.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
 
+// Function to perform activity selection
+void activitySelection(int start[], int finish[], int n) {
+    int i, j;
+
+    printf("Selected activities are:\n");
+    i = 0;  // First activity always selected
+    printf("A%d ", i + 1);
+
+    // Check the rest of the activities
+    for (j = 1; j < n; j++) {
+        if (start[j] >= finish[i]) {
+            printf("A%d ", j + 1);
+            i = j;
+        }
+    }
+}
+
+int main() {
+    int n, i, j, temp;
+    printf("Enter number of activities: ");
+    scanf("%d", &n);
+
+    int start[n], finish[n];
+
+    printf("Enter start times:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &start[i]);
+
+    printf("Enter finish times:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &finish[i]);
+
+    // Sort activities by finish time (ascending order)
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (finish[i] > finish[j]) {
+                // Swap finish time
+                temp = finish[i];
+                finish[i] = finish[j];
+                finish[j] = temp;
+
+                // Swap corresponding start time
+                temp = start[i];
+                start[i] = start[j];
+                start[j] = temp;
+            }
+        }
+    }
+
+    activitySelection(start, finish, n);
+    return 0;
+}
---
+++ DAA/huffman.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct MinHeapNode {
+    char data;
+    unsigned freq;
+    struct MinHeapNode *left, *right;
+};
+
+struct MinHeap {
+    unsigned size;
+    unsigned capacity;
+    struct MinHeapNode** array;
+};
+
+struct MinHeapNode* newNode(char data, unsigned freq) {
+    struct MinHeapNode* temp = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+    return temp;
+}
+
+struct MinHeap* createMinHeap(unsigned capacity) {
+    struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (struct MinHeapNode**)malloc(capacity * sizeof(struct MinHeapNode*));
+    return minHeap;
+}
+
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b) {
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void minHeapify(struct MinHeap* minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx) {
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+struct MinHeapNode* extractMin(struct MinHeap* minHeap) {
+    struct MinHeapNode* temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+    return temp;
+}
+
+void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode) {
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+    while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq) {
+        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    minHeap->array[i] = minHeapNode;
+}
+
+void printCodes(struct MinHeapNode* root, int arr[], int top) {
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+    if (!root->left && !root->right) {
+        printf("%c: ", root->data);
+        for (int i = 0; i < top; i++)
+            printf("%d", arr[i]);
+        printf("\n");
+    }
+}
+
+void HuffmanCodes(char data[], int freq[], int size) {
+    struct MinHeapNode *left, *right, *top;
+    struct MinHeap* minHeap = createMinHeap(size);
+
+    for (int i = 0; i < size; ++i)
+        minHeap->array[minHeap->size++] = newNode(data[i], freq[i]);
+
+    for (int i = (minHeap->size - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+
+    while (minHeap->size > 1) {
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+
+        insertMinHeap(minHeap, top);
+    }
+
+    int arr[100], topIndex = 0;
+    printCodes(minHeap->array[0], arr, topIndex);
+}
+
+int main() {
+    int n;
+    printf("Enter number of characters: ");
+    scanf("%d", &n);
+
+    char arr[n];
+    int freq[n];
+
+    printf("Enter characters:\n");
+    for (int i = 0; i < n; i++)
+        scanf(" %c", &arr[i]);
+
+    printf("Enter frequencies:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &freq[i]);
+
+    printf("\nHuffman Codes:\n");
+    HuffmanCodes(arr, freq, n);
+
+    return 0;
+}
